4e87cd811c721fa194dbd619e7d1590ea6065dbd
Alexey Parfenov <alepar@gmail.com>
2015-06-23 02:17:33 -0700

    jvm warmup

--- src/java/main/happycube/Main.java
+++ src/java/main/happycube/Main.java
@@ -63,16 +63,25 @@ public class Main {
         System.out.println("input\n\n" + printer + "--------------\n");
 
         Set<Cube> cubes = null;
-        final int iterations = 100;
-        final long start = System.nanoTime();
-        for (int i=0; i< iterations; i++) {
+        int iterations = 0;
+        long warmupEndNanos = 0;
+        long curNanos;
+        final long startNanos = System.nanoTime();
+        while ((curNanos = System.nanoTime()) < startNanos + 2_000_000_000L) {
+            if(warmupEndNanos == 0 && curNanos > startNanos + 1_000_000_000L) {
+                warmupEndNanos = curNanos;
+            }
+
             final CubeAssembler assembler = new CubeAssembler(5, sides);
             cubes = assembler.assemble();
-        }
-        final long end = System.nanoTime();
-        System.out.println();
 
-        System.out.println(cubes.size() + " unique cubes, took " + (end-start)/1000000/ iterations /1000.0 + "s per iter\n\n");
+            if (warmupEndNanos > 0) {
+                iterations++;
+            }
+        }
+        final long endNanos = System.nanoTime();
+
+        System.out.format("%d unique cubes, %.1fms to solve (%d iters in %.1fs)\n\n", cubes.size(), (endNanos - warmupEndNanos) / 100_000 / iterations / 10.0, iterations, (endNanos-warmupEndNanos)/100_000_000L/10.0);
         for (Cube cube : cubes) {
             System.out.println(cube);
             System.out.println("--------------\n");
